69b0dba2b1e96fcb158c364868196d51b4c108b8
Richard Mello <rgmello@inoa.com.br>
2026-03-29 19:37:20 -0300

    feat: poems gallery and view pages

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,9 @@
 import { defineCollection } from 'astro:content'
 import { z } from 'astro/zod'
+import { glob } from 'astro/loaders'
 
 const poemsCollection = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: "**/*.md", base: "./src/content/poems" }),
   schema: z.object({
     title: z.string(),
     author: z.string(),
